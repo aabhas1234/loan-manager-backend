90b6e1cb673323245296fb497abfa8b280148ffd
aabhas1234 <136249872+aabhas1234@users.noreply.github.com>
2025-03-27 16:32:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get('/api/getloanentries',getloanentries);
 app.post('/api/signin',signin );
 
 app.post('/api/signup',signup);
-
-app.listen(process.env.port,()=>{
-    console.log("app is successfully running ");
+const PORT=process.env.port|| 10000
+app.listen(PORT,"0.0.0.0",()=>{
+    console.log(`app is successfully running, on ${PORT} `);
 })
